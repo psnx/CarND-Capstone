02fb0e118ded7b146312f9e4d5606fee58e153b1
Toby <toby-coleman@users.noreply.github.com>
2017-10-27 11:44:38 +0100

    Better filter initialisation at steady state

--- ros/src/twist_controller/lowpass.py
+++ ros/src/twist_controller/lowpass.py
@@ -1,4 +1,4 @@
-from scipy.signal import butter, lfilter
+from scipy.signal import butter, lfilter, lfilter_zi
 import numpy as np
 from collections import deque
 
@@ -7,13 +7,17 @@ class LowPassFilter(object):
         # Set up Butterworth low pass filter
         self.b, self.a = butter(N, cutoff, btype='low', analog=False)
         self.x = deque(maxlen=len(self.b) - 1)
-        self.z = np.zeros(len(self.b) - 1)
+        self.z = None
         self.last_val = 0
 
     def get(self):
         return self.last_val
 
     def filt(self, val):
+        # Initialise if this is the first run
+        if self.z is None:
+            _ = [self.x.append(val) for idx in range(len(self.b) - 1)]
+            self.z = lfilter_zi(self.b, self.a) * val
         # Store new value in circular buffer
         self.x.append(val)
         # Filter
